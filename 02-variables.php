b289021a268e5201dac6acc448144a400d79437f
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-13 20:43:18 -0600

    Variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,13 @@
 <?php include 'includes/header.php';
 
+$name = "John Doe";
+echo "<h1>Hello, my name is $name</h1>";
+var_dump($name);
 
+define('constant', "This is a constant");
+echo constant;
 
+const constant2 = "This is a constant";
+echo constant2;
 
 include 'includes/footer.php';
